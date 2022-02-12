6165a1286f441c5281b6a37a8615fa337e05b998
santash2021 <89896454+santash2021@users.noreply.github.com>
2022-02-13 00:17:22 +0600

    Update after_install.sh
    asxasxa

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/server
-npm install
-npm install --save react react-dom react-scripts react-particles-js
-npm install pm2 -g
+#cd /home/ubuntu/server
+#npm install
+#npm install --save react react-dom react-scripts react-particles-js
+#npm install pm2 -g
